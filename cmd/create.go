feb2422429ea2d5be28cc1c5caa069c03146146b
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-05-14 08:33:11 +0100

    Switch to checkip.amazonaws.com for finding IP
    Sometimes ifconfig.co appears to fail and cause the service to
    try to bind to a null IP, which is confusing for users. It
    appears that the rate limit is one query per minute, on shared
    infrastructure it should be very rare that this happens.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

--- cmd/create.go
+++ cmd/create.go
@@ -383,7 +383,7 @@ curl -sLO https://raw.githubusercontent.com/inlets/inlets/master/hack/inlets-ope
 	return `#!/bin/bash
 export AUTHTOKEN="` + authToken + `"
 export REMOTETCP="` + remoteTCP + `"
-export IP=$(curl -sfSL https://ifconfig.co)
+export IP=$(curl -sfSL https://checkip.amazonaws.com)
 
 curl -SLsf https://github.com/inlets/inlets-pro/releases/download/0.6.0/inlets-pro > /tmp/inlets-pro && \
   chmod +x /tmp/inlets-pro  && \
