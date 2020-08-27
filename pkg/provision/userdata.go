594b78220d6e24816527486071264b005616cf5a
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2020-08-27 14:42:45 +0100

    Publish userdata code for use in inlets-operator
    Updated to take a version for inlets OSS / PRO
    
    Tested e2e with DigitalOcean with PRO + OSS, unit tests updated
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ pkg/provision/userdata.go
@@ -0,0 +1,43 @@
+package provision
+
+import "fmt"
+
+// MakeExitServerUserdata makes a user-data script in bash to setup inlets
+// using the OSS or PRO version with a systemd service
+func MakeExitServerUserdata(ossControlPort int, authToken, ossVersion, proVersion string, pro bool) string {
+	if pro {
+		return `#!/bin/bash
+export AUTHTOKEN="` + authToken + `"
+export IP=$(curl -sfSL https://checkip.amazonaws.com)
+
+curl -SLsf https://github.com/inlets/inlets-pro/releases/download/` + proVersion + `/inlets-pro > /tmp/inlets-pro && \
+  chmod +x /tmp/inlets-pro  && \
+  mv /tmp/inlets-pro /usr/local/bin/inlets-pro
+
+curl -sLO https://raw.githubusercontent.com/inlets/inlets-pro/master/artifacts/inlets-pro.service  && \
+  mv inlets-pro.service /etc/systemd/system/inlets-pro.service && \
+  echo "AUTHTOKEN=$AUTHTOKEN" >> /etc/default/inlets-pro && \
+  echo "IP=$IP" >> /etc/default/inlets-pro && \
+  systemctl start inlets-pro && \
+  systemctl enable inlets-pro
+`
+	}
+
+	controlPort := fmt.Sprintf("%d", ossControlPort)
+
+	return `#!/bin/bash
+export AUTHTOKEN="` + authToken + `"
+export CONTROLPORT="` + controlPort + `"
+
+curl -SLsf https://github.com/inlets/inlets/releases/download/` + ossVersion + `/inlets > /tmp/inlets && \
+chmod +x /tmp/inlets  && \
+mv /tmp/inlets /usr/local/bin/inlets
+
+curl -sLO https://raw.githubusercontent.com/inlets/inlets/master/hack/inlets.service && \
+mv inlets-operator.service /etc/systemd/system/inlets.service && \
+echo "AUTHTOKEN=$AUTHTOKEN" > /etc/default/inlets && \
+echo "CONTROLPORT=$CONTROLPORT" >> /etc/default/inlets && \
+systemctl start inlets && \
+systemctl enable inlets
+`
+}
---
+++ pkg/provision/userdata_test.go
@@ -0,0 +1,60 @@
+// Copyright (c) Inlets Author(s) 2020. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package provision
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func Test_makeUserdata_InletsOSS(t *testing.T) {
+	userData := MakeExitServerUserdata(8080, "auth", "2.7.4", "0.7.0", false)
+
+	wantUserdata := `#!/bin/bash
+export AUTHTOKEN="auth"
+export CONTROLPORT="8080"
+
+curl -SLsf https://github.com/inlets/inlets/releases/download/2.7.4/inlets > /tmp/inlets && \
+chmod +x /tmp/inlets  && \
+mv /tmp/inlets /usr/local/bin/inlets
+
+curl -sLO https://raw.githubusercontent.com/inlets/inlets/master/hack/inlets.service && \
+mv inlets-operator.service /etc/systemd/system/inlets.service && \
+echo "AUTHTOKEN=$AUTHTOKEN" > /etc/default/inlets && \
+echo "CONTROLPORT=$CONTROLPORT" >> /etc/default/inlets && \
+systemctl start inlets && \
+systemctl enable inlets
+`
+
+	ioutil.WriteFile("/tmp/oss", []byte(userData), 0600)
+
+	if userData != wantUserdata {
+		t.Errorf("want:\n%s\nbut got:\n%s", wantUserdata, userData)
+	}
+}
+
+func Test_makeUserdata_InletsPro(t *testing.T) {
+	userData := MakeExitServerUserdata(8080, "auth", "2.7.4", "0.7.0", true)
+
+	wantUserdata := `#!/bin/bash
+export AUTHTOKEN="auth"
+export IP=$(curl -sfSL https://checkip.amazonaws.com)
+
+curl -SLsf https://github.com/inlets/inlets-pro/releases/download/0.7.0/inlets-pro > /tmp/inlets-pro && \
+  chmod +x /tmp/inlets-pro  && \
+  mv /tmp/inlets-pro /usr/local/bin/inlets-pro
+
+curl -sLO https://raw.githubusercontent.com/inlets/inlets-pro/master/artifacts/inlets-pro.service  && \
+  mv inlets-pro.service /etc/systemd/system/inlets-pro.service && \
+  echo "AUTHTOKEN=$AUTHTOKEN" >> /etc/default/inlets-pro && \
+  echo "IP=$IP" >> /etc/default/inlets-pro && \
+  systemctl start inlets-pro && \
+  systemctl enable inlets-pro
+`
+
+	// ioutil.WriteFile("/tmp/pro", []byte(userData), 0600)
+	if userData != wantUserdata {
+		t.Errorf("want: %s, but got: %s", wantUserdata, userData)
+	}
+}
