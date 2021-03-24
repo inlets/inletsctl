f04acff49c401dc259bb8590c9629af36f7b2c73
Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>
2021-03-24 17:56:01 +0000

    Add HTTPS tunnel support for create command
    Tested with the staging issuer and a custom domain. This relies
    on a new service file, which will be published with each new
    inlets PRO binary release.
    
    inlets-pro-http.service is fetched and the defaults file is
    populated with the Let's Encrypt data such as domain, email
    and issuer.
    
    This enables fast HTTPS tunnels to be provisioned.
    
    Signed-off-by: Alex Ellis (OpenFaaS Ltd) <alexellis2@gmail.com>

---
+++ cmd/create_test.go
@@ -0,0 +1,65 @@
+// Copyright (c) Inlets Author(s) 2019. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+package cmd
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+func Test_MakeHTTPSUserdata_OneDomain(t *testing.T) {
+	got := MakeHTTPSUserdata("token", "0.8.4", "contact@example.com", "prod", []string{"example.com"})
+	ioutil.WriteFile("/tmp/t.txt", []byte(got), 0600)
+	want := `#!/bin/bash
+export AUTHTOKEN="token"
+export IP=$(curl -sfSL https://checkip.amazonaws.com)
+
+curl -SLsf https://github.com/inlets/inlets-pro/releases/download/0.8.4/inlets-pro -o /tmp/inlets-pro && \
+  chmod +x /tmp/inlets-pro  && \
+  mv /tmp/inlets-pro /usr/local/bin/inlets-pro
+
+curl -SLsf https://github.com/inlets/inlets-pro/releases/download/0.8.4/inlets-pro-http.service -o inlets-pro.service && \
+  mv inlets-pro.service /etc/systemd/system/inlets-pro.service && \
+  echo "AUTHTOKEN=$AUTHTOKEN" >> /etc/default/inlets-pro && \
+  echo "IP=$IP" >> /etc/default/inlets-pro && \
+  echo "DOMAINS=--letsencrypt-domain=example.com" >> /etc/default/inlets-pro && \
+  echo "ISSUER=--letsencrypt-issuer=prod" >> /etc/default/inlets-pro && \
+  echo "EMAIL=--letsencrypt-email=contact@example.com" >> /etc/default/inlets-pro && \
+  systemctl daemon-reload && \
+  systemctl start inlets-pro && \
+  systemctl enable inlets-pro
+`
+	if want != got {
+		t.Fatalf("want\n%s\nbut got\n%s\n", want, got)
+	}
+}
+
+func Test_MakeHTTPSUserdata_TwoDomains(t *testing.T) {
+	got := MakeHTTPSUserdata("token", "0.8.4", "contact@example.com", "prod",
+		[]string{"a.example.com", "b.example.com"})
+
+	ioutil.WriteFile("/tmp/t.txt", []byte(got), 0600)
+	want := `#!/bin/bash
+export AUTHTOKEN="token"
+export IP=$(curl -sfSL https://checkip.amazonaws.com)
+
+curl -SLsf https://github.com/inlets/inlets-pro/releases/download/0.8.4/inlets-pro -o /tmp/inlets-pro && \
+  chmod +x /tmp/inlets-pro  && \
+  mv /tmp/inlets-pro /usr/local/bin/inlets-pro
+
+curl -SLsf https://github.com/inlets/inlets-pro/releases/download/0.8.4/inlets-pro-http.service -o inlets-pro.service && \
+  mv inlets-pro.service /etc/systemd/system/inlets-pro.service && \
+  echo "AUTHTOKEN=$AUTHTOKEN" >> /etc/default/inlets-pro && \
+  echo "IP=$IP" >> /etc/default/inlets-pro && \
+  echo "DOMAINS=--letsencrypt-domain=a.example.com --letsencrypt-domain=b.example.com" >> /etc/default/inlets-pro && \
+  echo "ISSUER=--letsencrypt-issuer=prod" >> /etc/default/inlets-pro && \
+  echo "EMAIL=--letsencrypt-email=contact@example.com" >> /etc/default/inlets-pro && \
+  systemctl daemon-reload && \
+  systemctl start inlets-pro && \
+  systemctl enable inlets-pro
+`
+	if want != got {
+		t.Fatalf("want\n%s\nbut got\n%s\n", want, got)
+	}
+}
